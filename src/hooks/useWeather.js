ff8ffd5df877968cc55ce7451b80e40f71ca1b3d
Adam Brammer <abrammer89@gmail.com>
2023-05-20 15:21:41 -0400

    setWeather is not a function

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,37 +1,24 @@
 import { useState, useEffect } from 'react';
 
-const useWeather = (lat, lon) => {
+const useWeather = (lat, lon, apiKey) => {
   const [weather, setWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
   const [error, setError] = useState(null);
 
-  // Add these conversion functions
-  function convertTempToCelsius(kelvin) {
-    return (kelvin - 273.15).toFixed(2);
-  }
-
-  function convertTempToFahrenheit(kelvin) {
-    return ((kelvin - 273.15) * 9/5 + 32).toFixed(2);
-  }
-
   useEffect(() => {
     const fetchWeather = async () => {
       try {
-        // fetch weather data for today
-        const response = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`
+        // Fetch weather data
+        const weatherResponse = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
         );
-        const weatherData = await response.json();
-        weatherData.main.temp = convertTempToFahrenheit(weatherData.main.temp); // using Fahrenheit here
+        const weatherData = await weatherResponse.json();
 
-        // fetch weather forecast for the next 5 days
-        const responseForecast = await fetch(
-          `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`
+        // Fetch weather forecast
+        const forecastResponse = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
         );
-        const forecastData = await responseForecast.json();
-        forecastData.list.forEach(item => {
-          item.main.temp = convertTempToFahrenheit(item.main.temp); // using Fahrenheit here
-        });
+        const forecastData = await forecastResponse.json();
 
         setWeather(weatherData);
         setForecast(forecastData);
@@ -40,12 +27,12 @@ const useWeather = (lat, lon) => {
       }
     };
 
-    if (lat && lon) {
+    if (lat && lon && apiKey) {
       fetchWeather();
     }
-  }, [lat, lon]);
+  }, [lat, lon, apiKey]);
 
-  return { weather, forecast, error };
+  return { weather, forecast, setWeather, setForecast, error };
 };
 
 export default useWeather;
